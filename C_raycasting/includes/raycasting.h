35fe1ead544a5dbe172cda0422766a52d3d0959b
mhotting <madric.hottinger.101@gmail.com>
2024-06-06 16:07:26 +0200

    Basic intialization of the project

--- C_raycasting/includes/raycasting.h
+++ C_raycasting/includes/raycasting.h
@@ -1,4 +1,9 @@
 #ifndef RAYCASTING_H
 # define RAYCASTING_H
 
+# define WINDOW_TITLE	"Raycasting"
+# define WINDOW_WIDTH	800
+# define WINDOW_HEIGHT	600
+
+
 #endif
--- C_raycasting/srcs/raycasting.c
+++ C_raycasting/srcs/raycasting.c
@@ -1,7 +1,60 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+#include "raycasting.h"
+
+SDL_Window		*window = NULL;
+SDL_Renderer	*renderer = NULL;
+bool			is_game_running = false;
+
+bool	initialize_window(void)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		fprintf(stderr, "Error initializing SDL\n");
+		return (false);
+	}
+	window = SDL_CreateWindow(
+		WINDOW_TITLE,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		SDL_WINDOW_BORDERLESS
+	);
+	if (window == NULL)
+	{
+		fprintf(stderr, "Error creating SDL window\n");
+		return (false);
+	}
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer == NULL)
+	{
+		fprintf(stderr, "Error creating SDL renderer\n");
+		return (false);
+	}
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+	return (true);
+}
+
+void	destroy_window()
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
 
 int	main(void)
 {
-	printf("Hello World !\n");
+	is_game_running = initialize_window();
+	while (is_game_running)
+	{
+		//process_input();
+		//update();
+		//render();
+	}
+	destroy_window();
 	return (0);
 }
