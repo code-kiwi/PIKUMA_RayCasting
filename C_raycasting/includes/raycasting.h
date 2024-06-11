5f2943478ada61e80b32647e05acc6b6acafac4f
CodeKiwiFR <madric.hottinger.101@gmail.com>
2024-06-11 21:02:06 +0200

    Adds map render - Adds player render - Adds player movement handling (without collisions)

--- C_raycasting/includes/raycasting.h
+++ C_raycasting/includes/raycasting.h
@@ -1,11 +1,41 @@
 #ifndef RAYCASTING_H
 # define RAYCASTING_H
 
-# define WINDOW_TITLE	"Raycasting"
-# define WINDOW_WIDTH	800
-# define WINDOW_HEIGHT	600
+# define PI                     3.14159265
+# define TWO_PI                 6.28318530
 
-# define FPS            30
-# define FRAME_TIME_LEN (1000 / FPS) // time in ms for each frame
+# define TILE_SIZE              64
+# define MAP_NUM_ROWS           13
+# define MAP_NUM_COLS           20
+# define MINIMAP_SCALE_FACTOR   1.0
+
+# define WINDOW_TITLE	        "Raycasting"
+# define WINDOW_WIDTH	        (MAP_NUM_COLS * TILE_SIZE)
+# define WINDOW_HEIGHT	        (MAP_NUM_ROWS * TILE_SIZE)
+
+# define FOV_ANGLE              (60 * (PI / 180))
+# define NUM_RAYS               WINDOW_WIDTH
+
+# define PLAYER_DIR_LINE_LEN    40
+
+# define FPS                    30
+# define FRAME_TIME_LEN         (1000 / FPS) // time in ms for each frame
+
+typedef struct s_player t_player;
+
+struct s_player
+{
+    float   x;
+    float   y;
+    float   width;
+    float   height;
+    int     turn_direction; // -1 for left, 1 for right
+    int     walk_direction; // -1 for back, 1 for front
+    float   rotation_angle;
+    float   walk_speed;
+    float   turn_speed;
+};
+
+void move_player(float delta_time);
 
 #endif
--- C_raycasting/srcs/raycasting.c
+++ C_raycasting/srcs/raycasting.c
@@ -2,43 +2,31 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <math.h>
 
 #include "raycasting.h"
 
+const int map[MAP_NUM_ROWS][MAP_NUM_COLS] =
+{
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
 SDL_Window		*window = NULL;
 SDL_Renderer	*renderer = NULL;
 bool			is_game_running = false;
-
-int				playerX, playerY;
-
 int				ticks_last_frame = 0;
-
-void	setup(void)
-{
-	playerX = 0;
-	playerY = 0;
-}
-
-void	update(void)
-{
-	float	delta_time;
-	int		time_to_wait;
-
-	// Compute how long we have to wait until the reach of target frame time and wait if we are running to fast
-	time_to_wait = ticks_last_frame + FRAME_TIME_LEN - SDL_GetTicks();
-	if (time_to_wait > 0 && time_to_wait <= FRAME_TIME_LEN)
-		SDL_Delay(time_to_wait);
-
-	// Compute the delat time to be used as an update factor when changing game objects
-	delta_time = (SDL_GetTicks() - ticks_last_frame) / 1000.0f;
-
-	// Store the milliseconds of the current frame to be used in the future
-	ticks_last_frame = SDL_GetTicks();
-
-	// Update our objects
-	playerX += 32 * delta_time;
-	playerY += 32 * delta_time;
-}
+t_player		player;
 
 bool	initialize_window(void)
 {
@@ -77,6 +65,39 @@ void	destroy_window()
 	SDL_Quit();
 }
 
+void	setup(void)
+{
+	player.x = WINDOW_WIDTH / 2;
+	player.y = WINDOW_HEIGHT / 2;
+	player.width = 5;
+	player.height = 5;
+	player.turn_direction = 0;
+	player.walk_direction = 0;
+	player.rotation_angle = PI / 2;
+	player.walk_speed = 100; // pixels per second
+	player.turn_speed = 45 * (PI / 180); // radians per second
+}
+
+void	update(void)
+{
+	float	delta_time;
+	int		time_to_wait;
+
+	// Compute how long we have to wait until the reach of target frame time and wait if we are running to fast
+	time_to_wait = ticks_last_frame + FRAME_TIME_LEN - SDL_GetTicks();
+	if (time_to_wait > 0 && time_to_wait <= FRAME_TIME_LEN)
+		SDL_Delay(time_to_wait);
+
+	// Compute the delat time to be used as an update factor when changing game objects
+	delta_time = (SDL_GetTicks() - ticks_last_frame) / 1000.0f;
+
+	// Store the milliseconds of the current frame to be used in the future
+	ticks_last_frame = SDL_GetTicks();
+
+	// Update our objects as a function of delta time
+	move_player(delta_time);
+}
+
 void process_input()
 {
 	SDL_Event	event;
@@ -90,19 +111,94 @@ void process_input()
 		case SDL_KEYDOWN:
 			if (event.key.keysym.sym == SDLK_ESCAPE)
 				is_game_running = false;
+			else if (event.key.keysym.sym == SDLK_UP)
+				player.walk_direction = 1;
+			else if (event.key.keysym.sym == SDLK_DOWN)
+				player.walk_direction = -1;
+			else if (event.key.keysym.sym == SDLK_RIGHT)
+				player.turn_direction = 1;
+			else if (event.key.keysym.sym == SDLK_LEFT)
+				player.turn_direction = -1;
+			break ;
+		case SDL_KEYUP:
+			if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_DOWN)
+				player.walk_direction = 0;
+			else if (event.key.keysym.sym == SDLK_RIGHT || event.key.keysym.sym == SDLK_LEFT)
+				player.turn_direction = 0;
 			break ;
 	}
 }
 
+void move_player(float delta_time)
+{
+	float	move_step;
+	float	new_x, new_y;
+
+	// Handling rotation move
+	player.rotation_angle += player.turn_direction * player.turn_speed * delta_time;
+
+	// Handling walk move
+	move_step = player.walk_direction * player.walk_speed * delta_time;
+	new_x = player.x + cos(player.rotation_angle) * move_step;
+	new_y = player.y + sin(player.rotation_angle) * move_step;
+
+	player.x = new_x;
+	player.y = new_y;
+}
+
+void render_map()
+{
+	int			i, j;
+	int			tile_x, tile_y;
+	int			tile_color;
+	SDL_Rect	tile_rect;
+
+	for (i = 0; i < MAP_NUM_ROWS; i++)
+	{
+		for (j = 0; j < MAP_NUM_COLS; j++)
+		{
+			tile_x = j * TILE_SIZE;
+			tile_y = i * TILE_SIZE;
+			tile_color = map[i][j] != 0 ? 255 : 0;
+
+			SDL_SetRenderDrawColor(renderer, tile_color, tile_color, tile_color, 255);
+			tile_rect.h = TILE_SIZE * MINIMAP_SCALE_FACTOR;
+			tile_rect.w = TILE_SIZE * MINIMAP_SCALE_FACTOR;
+			tile_rect.x = tile_x * MINIMAP_SCALE_FACTOR;
+			tile_rect.y = tile_y * MINIMAP_SCALE_FACTOR;
+			SDL_RenderFillRect(renderer, &tile_rect);
+		}
+	}
+}
+
+void	render_player()
+{
+	SDL_Rect	player_rect;
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	player_rect.x = player.x * MINIMAP_SCALE_FACTOR;
+	player_rect.y = player.y * MINIMAP_SCALE_FACTOR;
+	player_rect.h = player.height * MINIMAP_SCALE_FACTOR;
+	player_rect.w = player.width * MINIMAP_SCALE_FACTOR;
+	SDL_RenderFillRect(renderer, &player_rect);
+
+	SDL_RenderDrawLine(
+		renderer,
+		player.x * MINIMAP_SCALE_FACTOR,
+		player.y * MINIMAP_SCALE_FACTOR,
+		(player.x + cos(player.rotation_angle) * PLAYER_DIR_LINE_LEN) * MINIMAP_SCALE_FACTOR,
+		(player.y + sin(player.rotation_angle) * PLAYER_DIR_LINE_LEN) * MINIMAP_SCALE_FACTOR
+	);
+}
+
 void render(void)
 {
-	SDL_Rect	rect = { playerX, playerY, 20, 20 };
-
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-	SDL_RenderFillRect(renderer, &rect);
+	render_map();
+	// render_rays();
+	render_player();
 
 	SDL_RenderPresent(renderer);
 }
@@ -110,6 +206,7 @@ void render(void)
 int	main(void)
 {
 	is_game_running = initialize_window();
+	setup();
 	while (is_game_running)
 	{
 		process_input();
