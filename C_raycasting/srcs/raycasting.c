19cd92c9927c9952246b6ed49e7e7681482f27bf
CodeKiwiFR <madric.hottinger.101@gmail.com>
2024-06-11 17:17:37 +0200

    Adds basic rendering for testing - Adds interaction in order to close the window

--- C_raycasting/srcs/raycasting.c
+++ C_raycasting/srcs/raycasting.c
@@ -9,6 +9,20 @@ SDL_Window		*window = NULL;
 SDL_Renderer	*renderer = NULL;
 bool			is_game_running = false;
 
+int				playerX, playerY;
+
+void	setup(void)
+{
+	playerX = 0;
+	playerY = 0;
+}
+
+void	update(void)
+{
+	playerX += 1;
+	playerY += 1;
+}
+
 bool	initialize_window(void)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
@@ -46,14 +60,44 @@ void	destroy_window()
 	SDL_Quit();
 }
 
+void process_input()
+{
+	SDL_Event	event;
+
+	SDL_PollEvent(&event);
+	switch (event.type)
+	{
+		case SDL_QUIT:
+			is_game_running = false;
+			break ;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				is_game_running = false;
+			break ;
+	}
+}
+
+void render(void)
+{
+	SDL_Rect	rect = { playerX, playerY, 20, 20 };
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+	SDL_RenderFillRect(renderer, &rect);
+
+	SDL_RenderPresent(renderer);
+}
+
 int	main(void)
 {
 	is_game_running = initialize_window();
 	while (is_game_running)
 	{
-		//process_input();
-		//update();
-		//render();
+		process_input();
+		update();
+		render();
 	}
 	destroy_window();
 	return (0);
